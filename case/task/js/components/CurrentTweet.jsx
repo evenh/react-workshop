0c15b271321d0e41ee59bbb92368d7b66caccc21
Even Holthe <even.holthe@me.com>
2015-09-08 15:04:57 +0200

    Optimalizing rendering of CurrentTweet

--- case/task/js/components/CurrentTweet.jsx
+++ case/task/js/components/CurrentTweet.jsx
@@ -2,6 +2,9 @@ var React = require('react');
 var Tweet = require('./Tweet');
 
 module.exports = React.createClass({
+    shouldComponentUpdate: function(nextProps, nextState) {
+      return nextProps.id !== this.props.id;
+    },
     render: function() {
       return (
         <div className="current-tweet">
