b66ed08279bd182e9997c83fec753e81ec184f56
Even Holthe <even.holthe@me.com>
2015-09-08 11:01:32 +0200

    Solved task 3

--- tasks/3/script.js
+++ tasks/3/script.js
@@ -1,3 +1,32 @@
 /*
     Exercise 3
 */
+
+var Timer = React.createClass({
+  getInitialState: function() {
+    return { timeAgo: 0, timer: undefined, startTime: new Date() };
+  },
+  setSecondsAgo: function(event) {
+    var diffMs = (new Date() - this.state.startTime);
+    this.setState({ timeAgo: (diffMs/1000) });
+  },
+  componentDidMount: function() {
+    this.setState({ timer: setInterval(this.setSecondsAgo, 100) });
+  },
+  componentWillUnmount() {
+    clearInterval(this.state.timer);
+    this.setState({ timer: undefined });
+  },
+
+  render: function() {
+    return (
+      <span>I was started {this.state.timeAgo} seconds ago</span>
+    );
+  }
+});
+
+// Render!
+React.render(
+  <Timer />,
+  document.body
+);
