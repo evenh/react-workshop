85a0ffd01ecbceed78f05ea3c882032c3ea47f01
Even Holthe <even.holthe@me.com>
2015-09-08 10:29:15 +0200

    Implemented solution for task 2

--- tasks/2/script.js
+++ tasks/2/script.js
@@ -1,3 +1,39 @@
 /*
     Exercise 2
 */
+
+// Components
+var Hello = React.createClass({
+  getDefaultProps: function() {
+    return {
+      name: 'World'
+    };
+  },
+
+  render: function() {
+    return (
+      <li>Hello, {this.props.name}</li>
+    );
+  }
+});
+
+var Helloes = React.createClass({
+  render: function() {
+    return (
+      <ul>
+        {this.props.names.map(function(nameValue){
+          return <Hello name={nameValue} />;
+        })}
+      </ul>
+    );
+  }
+});
+
+// Names to output
+var myNames = ['Even', 'Bjørnar'];
+
+// Render!
+React.render(
+  <Helloes names={myNames} />,
+  document.body
+);
